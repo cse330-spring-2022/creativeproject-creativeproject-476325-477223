c3e8b344a0af81a37d3f213026e91dc3b536a823
mcgatter <59850149+mcgatter@users.noreply.github.com>
2022-04-23 15:52:29 -0500

    pushing

---
+++ index.js
@@ -0,0 +1,27 @@
+const db = require('./db.js');
+const path = require('path')
+const express = require('express')
+const app = express()
+//require('dotenv').config();
+//db.connectDB();
+app.use(express.static(path.join(__dirname,"creative_project", "build")))
+//console.log(process.env.ATLAS_URI);
+app.use((req, res, next) => {
+    console.log('hiiiiii')
+    res.sendFile(path.join(__dirname, "react_files", 'build', 'index.html'))
+    
+})
+
+async function start(){
+    console.log('hello')
+  //  db = await db.connectDB()
+  let data = {hello:'maya'}
+  //await db.addToDB(data)
+}
+
+(async () => {
+    console.log('hello')
+    await start()
+}) ()
+
+app.listen(process.env.PORT, () => {console.log('Server is running')})
